2492a2ce0026cfff28a9b209dd878dc2afcdb841
Oshgnacknak <osh@oshgnacknak.de>
2021-08-18 14:53:05 +0200

    Open tournament from dialog/arg1/new

--- trfedit/TournamentPage.py
+++ trfedit/TournamentPage.py
@@ -4,12 +4,12 @@ from gi.repository import Gtk
 class TournamentPage(Gtk.Box):
     spacing = 10
 
-    def __init__(self, tournament, on_unsaved_changes):
+    def __init__(self, on_unsaved_changes):
         super().__init__(
             orientation=Gtk.Orientation.VERTICAL,
             spacing=self.spacing)
-        self.tournament = tournament
         self.on_unsaved_changes = on_unsaved_changes
+        self.tournament = None
 
         self.listbox = Gtk.ListBox()
         self.listbox.set_selection_mode(Gtk.SelectionMode.NONE)
@@ -19,45 +19,37 @@ class TournamentPage(Gtk.Box):
 
         self.name_entry = self.add_entry(
             'Tournament Name:',
-            tournament.name,
             self.on_name_changed)
 
         self.city_entry = self.add_entry(
             'Tournament City:',
-            tournament.city,
             self.on_city_changed)
 
         self.federation_entry = self.add_entry(
             'Tournament Federation:',
-            tournament.federation,
             self.on_federation_changed)
 
         self.startdate_entry = self.add_entry(
             'Tournament Start Date:',
-            tournament.startdate,
             self.on_startdate_changed)
 
         self.enddate_entry = self.add_entry(
             'Tournament End Date:',
-            tournament.enddate,
             self.on_enddate_changed)
 
         self.type_entry = self.add_entry(
             'Tournament Type:',
-            tournament.type,
             self.on_type_changed)
 
         self.chiefarbiter_entry = self.add_entry(
             'Tournament Chief Arbiter:',
-            tournament.chiefarbiter,
             self.on_chiefarbiter_changed)
 
         self.deputyarbiters_entry = self.add_entry(
             'Tournament Deputy Arbiters:',
-            tournament.deputyarbiters,
             self.on_deputyarbiters_changed)
 
-    def add_entry(self, label, text, on_changed):
+    def add_entry(self, label, on_changed):
         row = Gtk.ListBoxRow()
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         hbox.set_homogeneous(True)
@@ -67,13 +59,23 @@ class TournamentPage(Gtk.Box):
         hbox.pack_start(label, True, True, self.spacing)
 
         entry = Gtk.Entry()
-        entry.set_text(text)
         entry.connect('changed', on_changed)
         hbox.pack_start(entry, True, True, self.spacing)
 
         self.listbox.add(row)
 
         return entry
+
+    def set_tournament(self, tournament):
+        self.tournament = tournament
+        self.name_entry.set_text(tournament.name)
+        self.city_entry.set_text(tournament.city)
+        self.federation_entry.set_text(tournament.federation)
+        self.startdate_entry.set_text(tournament.startdate)
+        self.enddate_entry.set_text(tournament.enddate)
+        self.type_entry.set_text(tournament.type)
+        self.chiefarbiter_entry.set_text(tournament.chiefarbiter)
+        self.deputyarbiters_entry.set_text(tournament.deputyarbiters)
 
     def on_name_changed(self, widget):
         self.tournament.name = widget.get_text()
--- trfedit/Window.py
+++ trfedit/Window.py
@@ -1,4 +1,5 @@
 from gi.repository import Gtk
+import traceback
 import trf
 
 from .TournamentPage import TournamentPage
@@ -6,10 +7,10 @@ from .MenuBar import MenuBar
 
 
 class Window(Gtk.Window):
-    def __init__(self, tournament, tournament_path=None):
-        super().__init__(title=f'{tournament.name} - trfedit')
-        self.tournament = tournament
-        self.tournament_path = tournament_path
+    def __init__(self, tournament_path):
+        super().__init__(title='trfedit')
+        self.tournament = None
+        self.tournament_path = None
 
         menu_bar = MenuBar()
         self.add_accel_group(menu_bar.agr)
@@ -17,15 +18,15 @@ class Window(Gtk.Window):
         self.unsaved_changes = False
 
         menu_bar.add_menu('_File', [
-            (Gtk.STOCK_NEW,     '<Control>N',   self.on_file_new),
-            (Gtk.STOCK_OPEN,    '<Control>O',   self.on_file_open),
-            (Gtk.STOCK_SAVE,    '<Control>S',   self.on_file_save),
-            (Gtk.STOCK_SAVE_AS, None,           self.on_file_save_as),
-            (Gtk.STOCK_QUIT,    '<Control>Q',   self.on_quit)
+            (Gtk.STOCK_NEW,     '<Control>N',           self.on_file_new),
+            (Gtk.STOCK_OPEN,    '<Control>O',           self.on_file_open),
+            (Gtk.STOCK_SAVE,    '<Control>S',           self.on_file_save),
+            (Gtk.STOCK_SAVE_AS, '<Shift><Control>S',    self.on_file_save_as),
+            (Gtk.STOCK_QUIT,    '<Control>Q',           self.on_quit)
         ])
 
         self.notebook = Gtk.Notebook()
-        self.tournament_page = TournamentPage(tournament, self.on_unsaved_changes)
+        self.tournament_page = TournamentPage(self.on_unsaved_changes)
         self.notebook.append_page(
             self.tournament_page,
             Gtk.Label(label='Tournament'))
@@ -44,63 +45,22 @@ class Window(Gtk.Window):
         # teams: List[str] = field(default_factory=list)
         # xx_fields: Dict[str, str] = field(default_factory=dict)
 
+        if tournament_path is None:
+            self.set_tournament_to_new_tournament()
+        else:
+            self.set_tournament_by_path(tournament_path)
+
     def on_file_new(self, widget):
-        print('NEW FILE!')
+        self.ensure_changes_saved()
 
-    def on_file_open(self, widget):
-        print('OPEN FILE!')
+        path = self.ask_for_tournament_path()
+        if path is not None:
+            self.tournament_path = path
+            self.set_tournament_to_new_tournament()
+            self.update_title()
 
-    def on_file_save(self, widget):
-        self.save_tournament(self.tournament_path)
-
-    def on_file_save_as(self, widget):
-        self.save_tournament(None)
-
-    def save_tournament(self, path):
-        if path is None:
-            dialog = Gtk.FileChooserDialog(
-                'Please choose a file', self,
-                Gtk.FileChooserAction.SAVE,
-                (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-                    Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
-
-            response = dialog.run()
-            if response == Gtk.ResponseType.OK:
-                path = dialog.get_filename()
-
-            dialog.destroy()
-
-        if path is None:
-            return
-
-        try:
-            with open(path, 'w') as f:
-                trf.dump(f, self.tournament)
-        except Exception as e:
-            dialog = Gtk.MessageDialog(
-                transient_for=self,
-                flags=0,
-                message_type=Gtk.MessageType.ERROR,
-                buttons=Gtk.ButtonsType.CANCEL,
-                text='Error saving tournament')
-
-            dialog.format_secondary_text(str(e))
-            dialog.run()
-
-            dialog.destroy()
-            raise e
-
-        self.tournament_path = path
-        self.unsaved_changes = False
-
-    def on_unsaved_changes(self):
-        self.unsaved_changes = True
-
-    def on_quit(self, widget, event=None):
-        self.quit()
-
-    def quit(self):
-        if self.unsaved_changes:
+    def ensure_changes_saved(self):
+        while self.unsaved_changes:
             dialog = Gtk.MessageDialog(
                 transient_for=self,
                 flags=0,
@@ -114,7 +74,100 @@ class Window(Gtk.Window):
             response = dialog.run()
             if response == Gtk.ResponseType.YES:
                 self.save_tournament(self.tournament_path)
+            else:
+                self.unsaved_changes = False
 
             dialog.destroy()
 
+    def update_title(self):
+        self.set_title(self.tournament_path + ' - trfedit')
+
+    def on_file_open(self, widget):
+        self.ensure_changes_saved()
+
+        path = self.ask_for_tournament_path()
+        if path is not None:
+            self.set_tournament_by_path(path)
+
+    def on_file_save(self, widget):
+        self.save_tournament(self.tournament_path)
+
+    def on_file_save_as(self, widget):
+        self.save_tournament(None)
+
+    def save_tournament(self, path):
+        if path is None:
+            path = self.ask_for_tournament_path()
+
+        if path is None:
+            return
+
+        try:
+            with open(path, 'w') as f:
+                trf.dump(f, self.tournament)
+        except Exception as e:
+            self.show_error_dialog('Error writing tournament', e)
+            return
+
+        self.tournament_path = path
+        self.unsaved_changes = False
+
+    def ask_for_tournament_path(self):
+        path = None
+
+
+        dialog = Gtk.FileChooserDialog(
+            'Please choose a file', self,
+            Gtk.FileChooserAction.SAVE,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            path = dialog.get_filename()
+
+        dialog.destroy()
+        return path
+
+    def show_error_dialog(self, title, exception):
+        dialog = Gtk.MessageDialog(
+            transient_for=self,
+            flags=0,
+            message_type=Gtk.MessageType.ERROR,
+            buttons=Gtk.ButtonsType.CANCEL,
+            text=title)
+
+        dialog.format_secondary_text(str(exception))
+        dialog.run()
+
+        dialog.destroy()
+        traceback.print_exc()
+
+    def on_unsaved_changes(self):
+        self.unsaved_changes = True
+
+    def on_quit(self, widget, event=None):
+        self.quit()
+
+    def quit(self):
+        self.ensure_changes_saved()
         Gtk.main_quit()
+
+    def set_tournament(self, tournament):
+        self.tournament = tournament
+        self.tournament_page.set_tournament(tournament)
+        self.tournament_path = None
+
+    def set_tournament_to_new_tournament(self):
+        self.set_tournament(trf.Tournament(name='Unnamed Tournament'))
+
+    def set_tournament_by_path(self, path):
+        try:
+            with open(path, 'r') as f:
+                tour = trf.load(f)
+
+            self.set_tournament(tour)
+            self.tournament_path = path
+            self.update_title()
+        except Exception as e:
+            self.show_error_dialog('Error reading tournament', e)
